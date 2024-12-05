a63adf2fcc32d636321642c4ea88565589ba2cef
SpaceGangUno <144416246+SpaceGangUno@users.noreply.github.com>
2024-12-05 09:48:29 -0500

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import Hero from '../components/Hero';
 import Products from '../components/Products';
-import Process from '../components/Process';
 
 export default function Home() {
   return (
     <main className="flex-1">
       <Hero />
       <Products />
-      <Process />
     </main>
   );
 }
